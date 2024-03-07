6a3529e24e30e564f0386e2df7f2293928be87d9
Rodrigo de la Riva <100295171+Rodrigo-de-la-Riva@users.noreply.github.com>
2024-03-07 16:29:23 -0600

    boton cargar
    Se creo la funcion cargar, se vinculo al boton y se creo el espacio para leer el archivo cargado y mostrarlo en pantalla

--- Lexical_Analyzer/mainwindow.py
+++ Lexical_Analyzer/mainwindow.py
@@ -1,5 +1,5 @@
 from PyQt6.QtGui import QAction
-from PyQt6.QtWidgets import QMainWindow, QToolBar
+from PyQt6.QtWidgets import QMainWindow, QToolBar, QFileDialog, QLabel, QVBoxLayout, QWidget
 
 
 class MainWindow(QMainWindow):
@@ -8,13 +8,45 @@ class MainWindow(QMainWindow):
         self.setFixedSize(600, 500)
         self.setWindowTitle("Analizador")
 
+        # Variables para los labels
+        self.label = QLabel()
+
+        # Variables para el layout
+        self.main_layout = QVBoxLayout()
+        self.main_layout.addWidget(self.label)
+
+        # Variables para los widgets
+        self.main_widget = QWidget()
+        self.main_widget.setLayout(self.main_layout)
+        self.setCentralWidget(self.main_widget)
+
+        # Variables para la barra de tareas
         toolbar = QToolBar("toolbar")
         self.addToolBar(toolbar)
 
+        # Variables para los botones de la barra de tareas
+        # Boton para cargar un archivo de texto
         button_action1 = QAction("Cargar", self)
         button_action1.setStatusTip("Cargar un archivo de texto")
+        button_action1.triggered.connect(self.load)
         toolbar.addAction(button_action1)
 
+        # Boton para analizar el archivo de texto
         button_action2 = QAction("Analizar", self)
         button_action2.setStatusTip("Analiza el archivo cargado")
+        button_action1.triggered.connect(self.analizar)
         toolbar.addAction(button_action2)
+
+    def load(self):
+        dialog = QFileDialog()
+        dialog.setFileMode(QFileDialog.FileMode.ExistingFile)
+        dialog_succesful = dialog.exec()
+        if dialog_succesful:
+            file_location = dialog.selectedFiles()[0]
+            with open(file_location, 'rb') as file:
+                text = str(file.read()).split(sep="'")[1]
+                self.label.setText(text)
+                file.close()
+
+    def analizar(self):
+        pass
